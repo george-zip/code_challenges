39032ef8371ce2553606166f2bb5c5fb374d3e97
Chris Wallerstein <t@cmgwallerstein.net>
2021-08-29 21:33:43 +0000

    feat: Add atoi.py
    1. Iterate through input string and extract valid digits
    2. Generate the integer result by multiplying by increasing powers of 10.

---
+++ atoi.py
@@ -0,0 +1,46 @@
+"""
+State-machine-ish approach using the value of variables for 
+the implicit state 
+"""
+
+class Solution:
+	def myAtoi(self, s: str) -> int:
+		extracted = []
+		sign = 0
+		# extract valid digits, if any
+		for c in s:
+			if not sign:
+				if c.isspace():
+					continue
+				elif c in ("+", "-"):
+					sign = 1 if c == "+" else -1
+				elif c.isnumeric():
+					extracted.append(int(c))
+					sign = 1
+				else:
+					break
+			else:
+				if c.isnumeric():
+					extracted.append(int(c))
+				else:
+					break
+		val = 0
+		# generate integer result by multiplying digits 
+		# by increasing powers of 10
+		for i in range(len(extracted)):
+			n = extracted.pop()
+			val += n * 10 ** i
+		val *= sign
+		val = min(2 ** 31 - 1, val)
+		val = max(-2 ** 31, val)
+		return val
+
+
+
+s = Solution()
+print(s.myAtoi("42"))
+print(s.myAtoi("-42"))
+print(s.myAtoi("+42"))
+print(s.myAtoi("4193 with words"))
+print(s.myAtoi("words and 987"))
+print(s.myAtoi("-91283472332"))
